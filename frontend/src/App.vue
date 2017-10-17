bd64473f4cf34cdbbb224011595149b785dac0ea
puzzledPublic <puzzledPublic@gmail.com>
2017-10-17 22:39:04 +0900

    liveconfig css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,7 +12,12 @@ export default {
 </script>
 
 <style>
+html{
+  height: 100%;
+  background-image: linear-gradient(-128deg, rgba(255, 181, 32, 0.93) 3%, rgba(239, 39, 153, 0.93) 88%, rgba(237, 18, 171, 0.93) 100%);
+}
 #app {
+  
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- frontend/src/components/ExtensionLiveConfig.vue
+++ frontend/src/components/ExtensionLiveConfig.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="liveconfig">
-        <p>{{msg}}</p>
-        <input type="text" v-model="count"/><br>
-        <button @click="start">Start</button>
-        <button @click="terminate">Cancel</button>
+        <h3>{{msg}}</h3>
+        <div>
+            <div class="value">{{count}}</div>
+            <input type="range" min="1" max="30" v-model="count"/><br>
+            <button @click="start">Start</button>
+            <button @click="terminate">Cancel</button>
+        </div>
         <p>{{msg2}}</p>
         <div class="list">
             <p v-for="(list, index) in lists" :key="list">{{index+1}}. {{list}}</p>
@@ -67,3 +70,75 @@ export default {
     }
 }
 </script>
+<style>
+button{
+  background:#392e5c;
+  color:#fff;
+  border:none;
+  position:relative;
+  margin-top: 20px;
+  font-size:1.2em;
+  cursor:pointer;
+  transition:800ms ease all;
+  outline:none;
+}
+button:hover{
+  background:#fff;
+  color:#392e5c;
+}
+button:before,button:after{
+  content:'';
+  position:absolute;
+  top:0;
+  right:0;
+  height:2px;
+  width:0;
+  background: #392e5c;
+  transition:400ms ease all;
+}
+button:after{
+  right:inherit;
+  top:inherit;
+  left:0;
+  bottom:0;
+}
+button:hover:before,button:hover:after{
+  width:100%;
+  transition:800ms ease all;
+}
+
+.value {
+  text-align: center;
+  font-weight: bold;
+  font-size: 3em;
+  margin: 10px auto;
+  text-shadow: white 2px 2px 2px;
+}
+
+input[type="range"]{
+  -webkit-appearance:none;
+  width:200px;
+  height:2px;
+  background: white;
+  background-position:center;
+  background-repeat:no-repeat;
+  position: absolute;
+  top: 0px;
+  bottoM: 0px;
+  left: 0px;
+  right: 0px;
+  margin: auto;
+}
+
+input[type="range"]::-webkit-slider-thumb{
+  -webkit-appearance:none;
+  width:20px;
+  height:20px;
+  border-radius: 100%;
+  background: #392e5c;
+  position:relative;
+  border: 10px solid rgba(0,0,0,.3);
+  z-index:3;
+  cursor: pointer;
+}
+</style>
